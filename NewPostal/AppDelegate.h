3a6a1225945e5f911fd911fceee4d13f7b664143
hanxin <xin.han@pakpobox.com>
2019-08-05 09:59:41 +0800

    Initial Commit

---
+++ NewPostal/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  NewPostal
+//
+//  Created by mac on 2019/8/5.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
